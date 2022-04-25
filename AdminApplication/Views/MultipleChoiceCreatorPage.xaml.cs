d9af73e062eefc88ae75e8b89ea24fa59ba207bb
Bjarke Steffensen <bast38900@edu.ucl.dk>
2022-04-25 11:36:26 +0200

    MultipleChoiceCreator lavet...

---
+++ AdminApplication/Views/MultipleChoiceCreatorPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdminApplication.Views
+{
+    /// <summary>
+    /// Interaction logic for MultipleChoiceCreatorPage.xaml
+    /// </summary>
+    public partial class MultipleChoiceCreatorPage : Page
+    {
+        public MultipleChoiceCreatorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("Views/MultipleChoiceOverviewPage.xaml", UriKind.Relative));
+        }
+
+        private void ViewQuestion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Approve_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

bfa88fbbce8d582ca5692f8aef063ae0319bd338
Michael Søndergaard <m.soendergaard87@gmail.com>
2022-04-28 10:52:34 +0200

    Spørgsmåls knapper mm.

---
+++ AdminApplication/Views/QuestionCreatorPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdminApplication.Views
+{
+    /// <summary>
+    /// Interaction logic for QuestionCreatorPage.xaml
+    /// </summary>
+    public partial class QuestionCreatorPage : Page
+    {
+        public QuestionCreatorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

c1365d19d4a30006f2b93dc3a2a76d886c3e28f3
Matt Blomgrén Mauritsen <mbma37913@edu.ucl.dk>
2022-04-28 08:41:57 +0200

    Added ViewModel to Admin

---
+++ AdminApplication/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BuinsnessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminApplication.ViewModels
+{
+    public class QuestionViewModel
+    {
+        public static void Main()
+        {
+            List<Question> Questionlist = new List<Question>();
+
+
+            List<Maincategory> MaincategoryList = new List<Maincategory>();
+
+
+            List<Subcategory> SubcategoryList = new List<Subcategory>();
+        }
+    }
+}

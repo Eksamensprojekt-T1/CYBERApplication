74723e7f0251c7169777a152e97d0ce3c4b0dd2d
Phillip Friis-Alstrup <phillip.friis.alstrup@gmail.com>
2022-04-29 10:06:05 +0200

    Added "add method" to QuestionRepository

--- BuinsnessLogic/Persistence/QuestionRepository.cs
+++ BuinsnessLogic/Persistence/QuestionRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,47 @@ namespace BuinsnessLogic.Persistence
 {
     public class QuestionRepository : IRepository<Question>
     {
-        public int? Add(Question entity)
+        public List<Question> QuestionsList { get; set; } = new List<Question>();
+        private string connectionString;
+
+        public QuestionRepository(string connectionString)
         {
-            throw new NotImplementedException();
+            this.connectionString = connectionString;
+        }
+
+        public int? Add(Question question)
+        {
+            // Defining result
+            int? result = -1;
+
+            // Starts connection to database
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                // Defining SQL-Query
+                string table = "QUESTION";
+                string coloumns = "QuestionDescription, Difficulty"; // Mangler tilføjelse af kategorier og answers.
+                string values = "@QuestionDescription, @Difficulty";
+                string commandText =
+                    $"INSERT INTO {table} ({coloumns})" +
+                    $"VALUES ({values})";
+
+                // Making the call to the database
+                using (SqlCommand cmd = new SqlCommand(commandText, con))
+                {
+                    cmd.Parameters.Add("@QuestionDescription", SqlDbType.NVarChar).Value = question.QuestionDescription;
+                    cmd.Parameters.Add("@Difficulty", SqlDbType.NVarChar).Value = question.Difficulty;
+                    question.QuestionID = Convert.ToInt32(cmd.ExecuteScalar());
+                    result = question.QuestionID;
+                }
+
+                // Add Question to local list
+                QuestionsList.Add(question);
+
+                // returns result (current QuestionID)
+                return result;
+            }
         }
 
         public void Delete(int? entityID)

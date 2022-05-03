020a1e75a959d506cda00cda8fef1950ca7ae304
Michael Søndergaard <m.soendergaard87@gmail.com>
2022-05-03 11:07:59 +0200

    QuestionCreatorPage modified

--- AdminApplication/Views/QuestionCreatorPage.xaml.cs
+++ AdminApplication/Views/QuestionCreatorPage.xaml.cs
@@ -34,18 +34,18 @@ namespace AdminApplication.Views
             NavigationService.Navigate(new Uri("Views/QuestionOverviewPage.xaml", UriKind.Relative));
         }
 
-        private void Illustration_btn_Click(object sender, RoutedEventArgs e)
+        private void Picture_btn_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
-                Illustration_tb.Text = openFileDialog.FileName;
+                Picture_tb.Text = openFileDialog.FileName;
                 Uri test = new Uri(openFileDialog.FileName, UriKind.RelativeOrAbsolute);
-                Illustration_pt.Source = new BitmapImage(test);
+                Picture_pt.Source = new BitmapImage(test);
             }
         }
 
-        private void accept_btn_Click(object sender, RoutedEventArgs e)
+        private void Accept_btn_Click(object sender, RoutedEventArgs e)
         {
 
             string questionDescription = Title_tb.Text;
@@ -68,7 +68,12 @@ namespace AdminApplication.Views
             }
 
             QuestionVM.AddNewQuestion(questionDescription, (BuinsnessLogic.Models.Level)difficultyChosen);
-            MessageBox.Show("Spørgsmål oprettet!", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
+            MessageBox.Show("Spørgsmål oprettet!", "Meddelelse", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void CreateCategory_btn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }

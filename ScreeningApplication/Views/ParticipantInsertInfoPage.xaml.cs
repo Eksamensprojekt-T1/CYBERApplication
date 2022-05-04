c0ccac599b6d2d3c6159675baafc1a2a50288d64
Mads Friis <madsfriis91@gmail.com>
2022-05-04 14:02:59 +0200

    Velkomst og info page

---
+++ ScreeningApplication/Views/ParticipantInsertInfoPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreeningApplication.Views
+{
+    /// <summary>
+    /// Interaction logic for ParticipantInsertInfoPage.xaml
+    /// </summary>
+    public partial class ParticipantInsertInfoPage : Page
+    {
+        public ParticipantInsertInfoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearName(object sender, MouseButtonEventArgs e)
+        {
+            Name_tb.Clear();
+        }
+        private void ClearDOB(object sender, MouseButtonEventArgs e)
+        {
+            DOB_tb.Clear();
+        }
+        private void ClearGender(object sender, MouseButtonEventArgs e)
+        {
+            Gender_tb.Clear();
+        }
+        private void ClearEducation(object sender, MouseButtonEventArgs e)
+        {
+            Education_tb.Clear();
+        }
+    }
+}

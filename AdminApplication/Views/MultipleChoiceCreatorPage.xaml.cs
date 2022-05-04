afe62c62ca9ee60fa887fd88fa2f2d7fce0ca7b3
Bjarke Steffensen <bast38900@edu.ucl.dk>
2022-05-04 10:26:06 +0200

    datagrid viser spørsgmål

--- AdminApplication/Views/MultipleChoiceCreatorPage.xaml.cs
+++ AdminApplication/Views/MultipleChoiceCreatorPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdminApplication.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace AdminApplication.Views
     /// </summary>
     public partial class MultipleChoiceCreatorPage : Page
     {
+        QuestionViewModel qvm = new QuestionViewModel();
+        
         public MultipleChoiceCreatorPage()
         {
             InitializeComponent();
+            DataContext = qvm;
         }
 
         private void Back_btn_Click(object sender, RoutedEventArgs e)

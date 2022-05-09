a1d610494d908f03e0df77ae8cbf8d0e3d7975a2
Mads Friis <madsfriis91@gmail.com>
2022-05-09 13:42:08 +0200

    ParticipantInsertInfoViewModel start

--- ScreeningApplication/ViewModels/ParticipantInsertInfoViewModel.cs
+++ ScreeningApplication/ViewModels/ParticipantInsertInfoViewModel.cs
@@ -12,9 +12,17 @@ namespace ScreeningApplication.ViewModels
 {
     public class ParticipantInsertInfoViewModel
     {
+        readonly string connectionPath = Properties.Settings.Default.WPF_Connection;
+        private ParticipantRepository participantRepository;
         public ParticipantInsertInfoViewModel()
         {
+            participantRepository = new(connectionPath);
+        }
 
+        public void MakeAndLoadParticipant(string name, DateTime dob, string education, string gender, string motivation)
+        {
+            Participant participant = new(name, dob, education, gender, motivation);
+            participantRepository.Add(participant);
         }
     }
 }

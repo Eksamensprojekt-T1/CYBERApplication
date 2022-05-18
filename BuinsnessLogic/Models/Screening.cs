9c0494f6d8fd86abc1e7a21763ae07d0d8f0ee08
Bjarke Steffensen <bast38900@edu.ucl.dk>
2022-05-18 11:28:11 +0200

    vis sceening virker..

--- BuinsnessLogic/Models/Screening.cs
+++ BuinsnessLogic/Models/Screening.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
+using System.Timers;
 using System.Threading.Tasks;
 
 namespace BuinsnessLogic.Models
 {
     public class Screening
     {
+        private int screeningTimer;
         #region // Properties
 
         public int? ScreeningID { get; set; }
         public string ScreeningName { get; set; }
         public int ScreeningPassword { get; set; }
+        public double PassingScore { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public Timer ScreeningTimer { get; set; }
@@ -23,18 +25,20 @@ namespace BuinsnessLogic.Models
 
         #region // Constructors
 
-        public Screening(int? screeningID, string screeningName, int screeningPassword, DateTime startDate, DateTime endDate, Timer screeningTimer)
+        public Screening(int? screeningID, string screeningName, int screeningPassword, double passingScore, DateTime startDate, DateTime endDate, int screeningTimer)
         {
             ScreeningID = screeningID;
             ScreeningName = screeningName;
             ScreeningPassword = screeningPassword;
+            PassingScore = passingScore;
             StartDate = startDate;
             EndDate = endDate;
-            ScreeningTimer = screeningTimer;
+            this.screeningTimer = screeningTimer;
         }
 
-        public Screening(string screeningName, int screeningPassword, DateTime startDate, DateTime endDate, Timer screeningTimer)
-            : this(null, screeningName, screeningPassword, startDate, endDate, screeningTimer) { }
+        public Screening(string screeningName, int screeningPassword, double passingScore, DateTime startDate, DateTime endDate, int screeningTimer)
+            : this(null, screeningName, screeningPassword, passingScore, startDate, endDate, screeningTimer) { }
+
 
         #endregion
 
--- BuinsnessLogic/Persistence/ScreeningRepository.cs
+++ BuinsnessLogic/Persistence/ScreeningRepository.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Timers;
 using System.Threading.Tasks;
 using BuinsnessLogic.Models;
 
@@ -9,18 +12,60 @@ namespace BuinsnessLogic.Persistence
 {
     public class ScreeningRepository : IRepository<Screening>
     {
-        public List<Screening> Screenings { get; set; }
+       
+        //=========================================================================
+        // Fields & Properties
+        //=========================================================================
+        public List<Screening> ScreeningsList { get; set; } = new List<Screening>();
+        
         private string connectionString;
 
+        //=========================================================================
+        // Constructors
+        //=========================================================================
         public ScreeningRepository(string connectionString)
         {
-            Screenings = new();
             this.connectionString = connectionString;
+            loadAllEntitys();
         }
 
+        //=========================================================================
+        // Add (CRUD: Create)
+        // Adds a multiple choice to the database
+        //=========================================================================
         public int? Add(Screening screening)
         {
-            throw new NotImplementedException();
+            //Defining result
+            int? result = -1;
+
+            // Starts connection to database
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string table = "SCREENING";
+                string colums = "SCREENING.ScreeningName, SCREENING.ScreeningPassword, SCREENING.PassingScore, SCREENING.StartDate, SCREENING.EndDate, SCREENING.ScreeningTimer";
+                string values = "@ScreeningName, @ScreeningPassword, @PassingScore, @StartDate, @EndDate, @ScreeningTimer";
+                string commandText =
+                    $"INSERT INTO {table}({colums})" +
+                    $"VALUES ({values})" +
+                    $"SELECT @@IDENTITY";
+
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(commandText, con))
+                {
+                    cmd.Parameters.Add("@ScreeningName", SqlDbType.NVarChar).Value = screening.ScreeningName;
+                    cmd.Parameters.Add("@ScreeningPassword", SqlDbType.Int).Value = screening.ScreeningPassword;
+                    cmd.Parameters.Add("@PassingScore", SqlDbType.Float).Value = screening.PassingScore;
+                    cmd.Parameters.Add("@StartDate", SqlDbType.DateTime2).Value = screening.StartDate;
+                    cmd.Parameters.Add("@EndDate", SqlDbType.DateTime2).Value = screening.EndDate;
+                    cmd.Parameters.Add("@ScreeningTimer", SqlDbType.Int).Value = screening.ScreeningTimer;
+                    screening.ScreeningID = Convert.ToInt32(cmd.ExecuteScalar());
+                    result = screening.ScreeningID;
+                }
+
+                ScreeningsList.Add(screening);
+
+                return result;
+            }
         }
 
         public void Delete(int? screeningID)
@@ -30,7 +75,7 @@ namespace BuinsnessLogic.Persistence
 
         public IEnumerable<Screening> GetAll()
         {
-            throw new NotImplementedException();
+            return ScreeningsList;
         }
 
         public Screening GetByID(int? screeningID)
@@ -41,6 +86,43 @@ namespace BuinsnessLogic.Persistence
         public void Update(Screening screening)
         {
             throw new NotImplementedException();
+        }
+        
+        //=========================================================================
+        // LoadAllEntitys (CRUD: Read)
+        // Loads all objects from the MULTIPLECHOICE table in the database
+        // to the Repository list
+        //=========================================================================
+        private void loadAllEntitys()
+        {
+            using (SqlConnection con = new(connectionString))
+            {
+                string table = "SCREENING";
+                string values = "SCREENING.ScreeningID, SCREENING.ScreeningName, SCREENING.ScreeningPassword, SCREENING.PassingScore, SCREENING.StartDate, SCREENING.EndDate, SCREENING.ScreeningTimer";
+                string commandText = $"SELECT {values} FROM {table}";
+
+                con.Open();
+                using (SqlCommand sqlCommand = new(commandText, con))
+                {
+                    using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int? screeningID = int.Parse(reader["ScreeningID"].ToString());
+                            string screeningName = reader["ScreeningName"].ToString();
+                            int screeningPassword = int.Parse(reader["ScreeningPassword"].ToString());
+                            double passingScore = double.Parse(reader["PassingScore"].ToString());
+                            DateTime startDate = DateTime.Parse(reader["StartDate"].ToString());
+                            DateTime endDate = DateTime.Parse(reader["EndDate"].ToString());
+                            int screeningTimer = int.Parse(reader["ScreeningTimer"].ToString());
+
+                            Screening screening = new(screeningID, screeningName, screeningPassword, passingScore, startDate, endDate, screeningTimer);
+
+                            ScreeningsList.Add(screening); 
+                        }
+                    }
+                }
+            }
         }
     }
 }

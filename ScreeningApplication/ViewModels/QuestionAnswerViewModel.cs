1437aa07cba633a4c38d9ac991600465ab1bd358
Bjarke Steffensen <bast38900@edu.ucl.dk>
2022-05-18 09:01:00 +0200

    ..

--- ScreeningApplication/ViewModels/QuestionAnswerViewModel.cs
+++ ScreeningApplication/ViewModels/QuestionAnswerViewModel.cs
@@ -11,9 +11,32 @@ namespace ScreeningApplication.ViewModels
 {
     public class QuestionAnswerViewModel
     {
+        //=========================================================================
+        // Fields & Properties
+        //=========================================================================
+
+        // Connection string
+        static private string connectionString = Properties.Settings.Default.WPF_Connection;
+
+        // Observablecollections
+        public ObservableCollection<Question> QuestionVM { get; set; } = new ObservableCollection<Question>();
+        public ObservableCollection<Answer> AnswerVM { get; set; } = new ObservableCollection<Answer>();
+        public ObservableCollection<Picture> PictureVM { get; set; } = new ObservableCollection<Picture>();
+
+        // Repositories
+        QuestionRepository QuestionRepo = new QuestionRepository(connectionString);
+        AnswerRepository AnswerRepo = new AnswerRepository(connectionString);
+
         public QuestionAnswerViewModel()
         {
-
+            foreach (Question question in QuestionRepo.GetAll())
+            {
+                QuestionVM.Add(question);
+            }
+            foreach (Answer answer in AnswerRepo.GetAll())
+            {
+                AnswerVM.Add(answer);
+            }
         }
     }
 }
